2553c88d674fb48af41ffc8c060bbf15a4cd9347
Chenjia Li <cligrenet@gmail.com>
2022-08-16 10:21:31 +0200

    Modify site logo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import { React } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -9,11 +10,11 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	// </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
